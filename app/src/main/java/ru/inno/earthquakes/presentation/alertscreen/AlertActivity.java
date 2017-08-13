c3d9d0a4efca5da5dc8d6b9ac618e458d6180a0d
Artur Badretdinov <artursletter@gmail.com>
2017-08-13 21:18:16 +0300

    Fix Controllers and Activities issues after merge

--- app/src/main/java/ru/inno/earthquakes/presentation/alertscreen/AlertActivity.java
+++ app/src/main/java/ru/inno/earthquakes/presentation/alertscreen/AlertActivity.java
@@ -12,15 +12,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
-
+import com.google.android.gms.common.GoogleApiAvailability;
 import java.util.Locale;
-
 import javax.inject.Inject;
-
 import ru.inno.earthquakes.EartquakeApp;
 import ru.inno.earthquakes.R;
 import ru.inno.earthquakes.entities.EarthquakeWithDist;
@@ -33,128 +30,142 @@ import ru.inno.earthquakes.presentation.earthquakeslist.EarthquakesListActivity;
 import ru.inno.earthquakes.presentation.settings.SettingsActivity;
 
 /**
- * @author Artur Badretdinov (Gaket)
- *         22.07.17
+ * @author Artur Badretdinov (Gaket) 22.07.17
  */
 public class AlertActivity extends MvpAppCompatActivity implements AlertView {
 
-    // Here, and in other controllers, controller works as a root for some model components
-    // dependency tree. As a result, we inject them here and deeper all injections are made
-    // through the constructors.
-    @InjectPresenter
-    AlertPresenter presenter;
-    @Inject
-    EarthquakesInteractor earthquakesInteractor;
-    @Inject
-    LocationInteractor locationInteractor;
-    @Inject
-    SettingsInteractor settinsInteractor;
-    @Inject
-    SchedulersProvider schedulersProvider;
+  // Here, and in other controllers, controller works as a root for some model components
+  // dependency tree. As a result, we inject them here and deeper all injections are made
+  // through the constructors.
+  @InjectPresenter
+  AlertPresenter presenter;
+  @Inject
+  EarthquakesInteractor earthquakesInteractor;
+  @Inject
+  LocationInteractor locationInteractor;
+  @Inject
+  SettingsInteractor settinsInteractor;
+  @Inject
+  SchedulersProvider schedulersProvider;
+  @Inject
+  GoogleApiAvailability googleApiAvailability;
 
-    @ProvidePresenter
-    AlertPresenter providePresenter() {
-        return new AlertPresenter(earthquakesInteractor, locationInteractor, settinsInteractor, schedulersProvider);
+  @ProvidePresenter
+  AlertPresenter providePresenter() {
+    return new AlertPresenter(earthquakesInteractor, locationInteractor, settinsInteractor,
+        schedulersProvider);
+  }
+
+  private SwipeRefreshLayout swipeRefreshLayout;
+  private TextView messageView;
+  private TextView detailsView;
+  private TextView magnitudeView;
+  private TextView distanceView;
+  private ImageView alertImageView;
+  private Snackbar snackbar;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    EartquakeApp.getComponentsManager().getEarthquakesComponent().inject(this);
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+    messageView = (TextView) findViewById(R.id.alert_message);
+    detailsView = (TextView) findViewById(R.id.alert_details);
+    magnitudeView = (TextView) findViewById(R.id.alert_magnitude);
+    distanceView = (TextView) findViewById(R.id.alert_distance);
+    alertImageView = (ImageView) findViewById(R.id.alert_status);
+    swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.alert_swipe_refresh);
+    swipeRefreshLayout.setOnRefreshListener(() -> presenter.onRefreshAction());
+    findViewById(R.id.alert_show_all).setOnClickListener(v -> presenter.onShowAll());
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_alert, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_settings:
+        presenter.onOpenSettings();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
+  }
 
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private TextView messageView;
-    private TextView detailsView;
-    private TextView magnitudeView;
-    private TextView distanceView;
-    private ImageView alertImageView;
-    private Snackbar snackbar;
+  @Override
+  public void showThereAreNoAlerts() {
+    messageView.setText(R.string.alert_msg_everything_is_ok);
+    alertImageView.setImageResource(R.drawable.earth_normal);
+    distanceView.setVisibility(View.GONE);
+    magnitudeView.setVisibility(View.GONE);
+    detailsView.setText(R.string.alert_details_no_earthquakes);
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        EartquakeApp.getComponentsManager().getEarthquakesComponent().inject(this);
-        super.onCreate(savedInstanceState);
+  @Override
+  public void showEarthquakeAlert(EarthquakeWithDist earthquake) {
+    alertImageView.setImageResource(R.drawable.earth_alarm);
+    messageView.setText(R.string.alert_msg_earthquake_nearby);
+    detailsView.setText(earthquake.getEarthquake().getTitle());
+    distanceView.setText(getResources().getString(R.string.alert_distance_from_place,
+        Utils.formatDistanceString(earthquake.getDistance())));
+    String magnitude = String
+        .format(Locale.getDefault(), "%.2f", earthquake.getEarthquake().getMagnitude());
+    magnitudeView.setText(magnitude);
+    distanceView.setVisibility(View.VISIBLE);
+    magnitudeView.setVisibility(View.VISIBLE);
+  }
 
-        setContentView(R.layout.activity_main);
-        messageView = (TextView) findViewById(R.id.alert_message);
-        detailsView = (TextView) findViewById(R.id.alert_details);
-        magnitudeView = (TextView) findViewById(R.id.alert_magnitude);
-        distanceView = (TextView) findViewById(R.id.alert_distance);
-        alertImageView = (ImageView) findViewById(R.id.alert_status);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.alert_swipe_refresh);
-        swipeRefreshLayout.setOnRefreshListener(() -> presenter.onRefreshAction());
-        findViewById(R.id.alert_show_all).setOnClickListener(v -> presenter.onShowAll());
+  @Override
+  public void showNetworkError(boolean show) {
+    if (show) {
+      snackbar = Snackbar.make(swipeRefreshLayout, R.string.error_connection,
+          BaseTransientBottomBar.LENGTH_INDEFINITE)
+          .setAction(R.string.action_ok, (d) -> snackbar.dismiss());
+      snackbar.show();
+    } else if (snackbar != null) {
+      snackbar.dismiss();
     }
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_alert, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+  @Override
+  public void showLoading(boolean show) {
+    swipeRefreshLayout.setRefreshing(show);
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                presenter.onOpenSettings();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+  @Override
+  public void navigateToEarthquakesList() {
+    Intent intent = EarthquakesListActivity.getStartIntent(this);
+    startActivity(intent);
+  }
 
-    @Override
-    public void showThereAreNoAlerts() {
-        messageView.setText(R.string.alert_msg_everything_is_ok);
-        alertImageView.setImageResource(R.drawable.earth_normal);
-        detailsView.setVisibility(View.GONE);
-        distanceView.setVisibility(View.GONE);
-        magnitudeView.setVisibility(View.GONE);
-    }
+  @Override
+  public void navigateToSettings() {
+    Intent intent = SettingsActivity.getStartIntent(this);
+    startActivity(intent);
+  }
 
-    @Override
-    public void showEartquakeAlert(EarthquakeWithDist earthquake) {
-        alertImageView.setImageResource(R.drawable.earth_alarm);
-        messageView.setText(R.string.alert_msg_earhquake_nearby);
-        detailsView.setText(earthquake.getEarthquake().getTitle());
-        distanceView.setText(String.format("\u2248 %s km from you", Utils.formatDistanceString(earthquake.getDistance())));
-        String magnitude = String.format(Locale.getDefault(), "%.2f", earthquake.getEarthquake().getMagnitude());
-        magnitudeView.setText(magnitude);
-        detailsView.setVisibility(View.VISIBLE);
-        distanceView.setVisibility(View.VISIBLE);
-        magnitudeView.setVisibility(View.VISIBLE);
-    }
+  @Override
+  public void showPermissionDeniedAlert() {
+    Toast.makeText(this, R.string.error_prohibited_location_access, Toast.LENGTH_LONG).show();
+  }
 
-    @Override
-    public void showNetworkError(boolean show) {
-        if (show) {
-            snackbar = Snackbar.make(swipeRefreshLayout, R.string.error_connection, BaseTransientBottomBar.LENGTH_INDEFINITE)
-                    .setAction(R.string.action_ok, (d) -> snackbar.dismiss());
-            snackbar.show();
-        } else if (snackbar != null) {
-            snackbar.dismiss();
-        }
-    }
+  @Override
+  public void showNoDataAlert() {
+    Toast.makeText(this, R.string.error_no_data, Toast.LENGTH_LONG).show();
+  }
 
-    @Override
-    public void showLoading(boolean show) {
-        swipeRefreshLayout.setRefreshing(show);
+  @Override
+  public void showGoogleApiMessage(int status) {
+    if (googleApiAvailability.isUserResolvableError(status)) {
+      googleApiAvailability.getErrorDialog(this, status, 1).show();
+    } else {
+      Snackbar.make(swipeRefreshLayout, R.string.error_google_api_unavailable,
+          Snackbar.LENGTH_INDEFINITE).show();
     }
-
-    @Override
-    public void navigateToEarthquakesList() {
-        Intent intent = EarthquakesListActivity.getStartIntent(this);
-        startActivity(intent);
-    }
-
-    @Override
-    public void navigateToSettings() {
-        Intent intent = SettingsActivity.getStartIntent(this);
-        startActivity(intent);
-    }
-
-    @Override
-    public void showPermissionDeniedAlert() {
-        Toast.makeText(this, R.string.error_prohibited_location_access, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void showNoDataAlert() {
-        Toast.makeText(this, R.string.error_no_data, Toast.LENGTH_LONG).show();
-    }
+  }
 }
--- app/src/main/java/ru/inno/earthquakes/presentation/settings/SettingsActivity.java
+++ app/src/main/java/ru/inno/earthquakes/presentation/settings/SettingsActivity.java
@@ -1,5 +1,7 @@
 package ru.inno.earthquakes.presentation.settings;
 
+import static android.view.inputmethod.EditorInfo.IME_ACTION_DONE;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,132 +11,140 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
-
 import java.util.Locale;
-
 import javax.inject.Inject;
-
 import ru.inno.earthquakes.EartquakeApp;
 import ru.inno.earthquakes.R;
 import ru.inno.earthquakes.model.settings.SettingsInteractor;
 import ru.inno.earthquakes.presentation.info.InfoActivity;
 
-import static android.view.inputmethod.EditorInfo.IME_ACTION_DONE;
-
 public class SettingsActivity extends MvpAppCompatActivity implements SettingsView {
 
-    @InjectPresenter
-    SettingsPresenter presenter;
-    @Inject
-    SettingsInteractor settingsInteractor;
+  @InjectPresenter
+  SettingsPresenter presenter;
+  @Inject
+  SettingsInteractor settingsInteractor;
 
-    private EditText distanceView;
-    private SeekBar magnitudeView;
-    private TextView magnitudeValueView;
+  private EditText distanceView;
+  private SeekBar magnitudeView;
+  private TextView magnitudeValueView;
+  private TextView defaultCityView;
 
-    @ProvidePresenter
-    SettingsPresenter providePresenter() {
-        EartquakeApp.getComponentsManager().getAppComponent().inject(this);
-        return new SettingsPresenter(settingsInteractor);
-    }
+  @ProvidePresenter
+  SettingsPresenter providePresenter() {
+    EartquakeApp.getComponentsManager().getAppComponent().inject(this);
+    return new SettingsPresenter(settingsInteractor);
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // According to YAGNI, we have only some hardcoded views here.
-        // If application becomes more complicated, here should be a RecyclerView with options.
-        distanceView = (EditText) findViewById(R.id.settings_distance);
-        distanceView.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == IME_ACTION_DONE) {
-                saveSettings();
-                return true;
-            } else {
-                return false;
-            }
-        });
+    // According to YAGNI, we have only some hardcoded views here.
+    // If application becomes more complicated, here should be a RecyclerView with options.
+    defaultCityView = (TextView) findViewById(R.id.settings_default_city);
+    distanceView = (EditText) findViewById(R.id.settings_distance);
+    distanceView.setOnEditorActionListener((v, actionId, event) -> {
+      if (actionId == IME_ACTION_DONE) {
+        saveSettings();
+        return true;
+      } else {
+        return false;
+      }
+    });
 
-        magnitudeValueView = (TextView) findViewById(R.id.settings_magnitude_value);
-        magnitudeView = (SeekBar) findViewById(R.id.settings_magnitude);
-        magnitudeView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                magnitudeValueView.setText(String.format(Locale.getDefault(), "%.1f", progress / 10.0));
-            }
+    magnitudeValueView = (TextView) findViewById(R.id.settings_magnitude_value);
+    magnitudeView = (SeekBar) findViewById(R.id.settings_magnitude);
+    magnitudeView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        magnitudeValueView.setText(String.format(Locale.getDefault(), "%.1f", progress / 10.0));
+      }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // do nothing
-            }
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+        // do nothing
+      }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // do nothing
-            }
-        });
-        findViewById(R.id.settings_bt_save).setOnClickListener(v -> {
-            saveSettings();
-        });
-    }
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        // do nothing
+      }
+    });
+    findViewById(R.id.settings_bt_save).setOnClickListener(v -> {
+      saveSettings();
+    });
+  }
 
-    private void saveSettings() {
-        double magnitude = magnitudeView.getProgress() / 10.0;
-        presenter.onSave(distanceView.getText().toString(), magnitude);
-    }
+  private void saveSettings() {
+    String dist = distanceView.getText().toString();
+    double magnitude = magnitudeView.getProgress() / 10.0;
+    presenter.onSave(dist, magnitude);
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_settings, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_info:
-                presenter.onInfoAction();
-                return true;
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void setMaxDistance(Double dist) {
-        distanceView.setText(String.format(Locale.getDefault(), "%.0f", dist));
-    }
-
-    @Override
-    public void setMinMagnitude(Double mag) {
-        magnitudeView.setProgress((int) (mag * 10));
-    }
-
-    @Override
-    public void showDistanceFormatError() {
-        distanceView.setError(getResources().getString(R.string.error_settings_distance));
-    }
-
-    @Override
-    public void close() {
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_info:
+        presenter.onInfoAction();
+        return true;
+      case android.R.id.home:
         onBackPressed();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
+  }
 
-    @Override
-    public void navigateToInfo() {
-        Intent intent = InfoActivity.getStartInfo(this);
-        startActivity(intent);
-    }
+  @Override
+  public void setMaxDistance(Double dist) {
+    distanceView.setText(String.format(Locale.getDefault(), "%.0f", dist));
+  }
 
-    public static Intent getStartIntent(Context callingContext) {
-        return new Intent(callingContext, SettingsActivity.class);
-    }
+  @Override
+  public void setMinMagnitude(Double mag) {
+    magnitudeView.setProgress((int) (mag * 10));
+  }
+
+  @Override
+  public void showDistanceFormatError() {
+    distanceView.setError(getResources().getString(R.string.error_settings_distance));
+  }
+
+  @Override
+  public void setDefaultCity(String name) {
+    defaultCityView.setText(name);
+  }
+
+  @Override
+  public void showNotImplementedError() {
+    Toast.makeText(this, R.string.error_not_implemented, Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void close() {
+    onBackPressed();
+  }
+
+  @Override
+  public void navigateToInfo() {
+    Intent intent = InfoActivity.getStartInfo(this);
+    startActivity(intent);
+  }
+
+  public static Intent getStartIntent(Context callingContext) {
+    return new Intent(callingContext, SettingsActivity.class);
+  }
 }
